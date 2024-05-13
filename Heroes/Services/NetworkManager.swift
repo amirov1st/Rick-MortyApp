c68fe68e37e33fea3bb338dbbc79cd78aa8e86b7
Amirov Foma <amirov.foma@icloud.com>
2024-05-13 23:17:35 +0300

    feat: NetworkManager is done.

--- Heroes/Services/NetworkManager.swift
+++ Heroes/Services/NetworkManager.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
+enum Link: String {
+    case url = "https://rickandmortyapi.com/api/character"
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func fetchImage(url: String, completion: @escaping (Data) -> Void) {
+        guard let url = URL(string: url) else { return }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                completion(imageData)
+            }
+        }
+    }
+    
+    func fetchCharacters(url: String, completion: @escaping (Result<[Character], NetworkError>) -> Void) {
+        guard let url = URL(string: url) else { completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else { completion(.failure(.noData))
+                return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let charactersQuery = try decoder.decode(CharactersQuery.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(charactersQuery.results))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+    
+    private init() {}
+}
