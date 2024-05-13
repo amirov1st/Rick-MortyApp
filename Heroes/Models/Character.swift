882173ab70fb00ec465d885f462517e520ef7de6
Amirov Foma <amirov.foma@icloud.com>
2024-05-13 23:16:47 +0300

    feat: Model is done.

---
+++ Heroes/Models/Character.swift
@@ -0,0 +1,28 @@
+//
+//  Character.swift
+//  Heroes
+//
+//  Created by Amirov Foma on 13.05.2024.
+//
+
+struct CharactersQuery: Decodable {
+    let results: [Character]
+}
+
+struct Character: Decodable {
+    let id: Int
+    let name: String
+    let species: String
+    let gender: String
+    let origin: Origin
+    let location: Location
+    let image: String
+}
+
+struct Origin: Decodable {
+    let name: String
+}
+
+struct Location: Decodable {
+    let name: String
+}
