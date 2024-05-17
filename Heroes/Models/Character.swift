d7dad5bc8383a93ca434506acbb28bfb197fd6cf
Amirov Foma <amirov.foma@icloud.com>
2024-05-17 11:50:01 +0300

    feat: Model parced manually

--- Heroes/Models/Character.swift
+++ Heroes/Models/Character.swift
@@ -17,8 +17,35 @@ struct Character: Decodable {
     let origin: Origin
     let location: Origin
     let image: String
+    
+    init(value: [String: Any]) {
+        id = value["id"] as? Int ?? 0
+        name = value["name"] as? String ?? ""
+        species = value["species"] as? String ?? ""
+        gender = value["gender"] as? String ?? ""
+        origin = Origin(value: value["origin"] as? [String : String] ?? [:])
+        location = Origin(value: value["location"] as? [String: String] ?? [:])
+        image = value["image"] as? String ?? ""
+    }
+    
+    static func getCharacters(from value: Any) -> [Character] {
+        guard let value = value as? [String: Any] else { return [] }
+        guard let results = value["results"] as? [[String: Any]] else { return [] }
+        var characters = [Character]()
+        
+        for result in results {
+            let character = Character(value: result)
+            characters.append(character)
+        }
+        
+        return characters
+    }
 }
 
 struct Origin: Decodable {
     let name: String
+    
+    init(value: [String: String]) {
+        name = value["name"] ?? ""
+    }
 }
